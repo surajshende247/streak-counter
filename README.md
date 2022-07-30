2da37f266a2069b0fe67984e5874190a9d3bfc62
Suraj Shende <surajshende247@gmail.com>
2022-07-30 15:23:44 +0530

    first release

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# streak-counter
+# `@surajshende247/streak-counter` - a basic streak counter
+
+This is a basic streak counter - inspired by Duolingo - written in TypeScript and meant for the browser (uses `localStorage`).
+
+## Install
+
+```shell
+yarn add @surajshende247/streak-counter
+```
+
+npm install @surajshende247/streak-counter
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { Streak, formattedDate, buildStreak, KEY, updateStreak } from "./utils";
+
+function shouldIncrementOrResetStreakCount(
+  currentDate: Date,
+  lastLoginDate: string
+): "increment" | "reset" | "none" {
+  // We get 11/5/2021
+  // so to get 5, we split on / and get the second item
+  const difference =
+    currentDate.getDate() - parseInt(lastLoginDate.split("/")[1]);
+
+  // Same-day login, do nothing
+  if (difference === 0) {
+    return "none";
+  }
+
+  // This means they logged in the day after the currentDate
+  if (difference === 1) {
+    return "increment";
+  }
+  // Otherwise they logged in after a day, which would
+  // break the streak
+  return "reset";
+}
+
+export function streakCounter(storage: Storage, date: Date) {
+  const streakInLocalStorage = storage.getItem(KEY);
+  if (streakInLocalStorage) {
+    try {
+      const streak = JSON.parse(streakInLocalStorage) as Streak;
+
+      const state = shouldIncrementOrResetStreakCount(
+        date,
+        streak.lastLoginDate
+      );
+
+      const SHOULD_INCREMENT = state === "increment";
+      const SHOULD_RESET = state === "reset";
+
+      if (SHOULD_INCREMENT) {
+        const updatedStreak = buildStreak(date, {
+          startDate: streak.startDate,
+          currentCount: streak.currentCount + 1,
+          lastLoginDate: formattedDate(date),
+        });
+
+        updateStreak(storage, updatedStreak);
+        return updatedStreak;
+      }
+
+      if (SHOULD_RESET) {
+        const updatedStreak = buildStreak(date);
+
+        updateStreak(storage, updatedStreak);
+        return updatedStreak;
+      }
+
+      return streak;
+    } catch (error) {
+      console.error("Failed to parse streak from localStorage");
+    }
+  }
+
+  const streak = buildStreak(date);
+
+  updateStreak(storage, streak);
+
+  return streak;
+}
